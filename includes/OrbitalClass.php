c0da617e79bf7d8c8b71a38a0f7a7d0b9cbe3a77
lwalangitan <lwalangitan@rapiddg.com>
2015-05-23 09:46:36 -0500

    drupal coding standards, remove deprecated variables, add error message when no response code is given

--- includes/OrbitalClass.php
+++ includes/OrbitalClass.php
@@ -1,175 +1,181 @@
 <?php
 
-class OrbitalProcessor {
-  const AUTH_APPROVED = 1;
-  const AUTH_DECLINED = 2;
-  const AUTH_ERROR = 3;
+/**
+ * @file
+ * @class UcOrbitalGateway
+ */
 
-  protected $mode = "test"; //in test mode we can try things out without making any real transactions
+/**
+ * Chase Paymentech Orbital Gateway Class.
+ */
+class UcOrbitalGateway {
 
-  public $industry_type;
-  public $message_type;
+  public $industryType;
+  public $messageType;
   public $bin;
-  public $merchant_id;
-  public $terminal_id;
-  public $cc_num;
-  public $cc_expiry_mon;
-  public $cc_expiry_yr;
-  public $currency_code;
-  public $curency_exp;
+  public $ccNumber;
+  public $ccExpireMonth;
+  public $ccExpireYear;
+  public $currencyCode;
   public $cvv;
-  public $postal_code;
+  public $postalCode;
   public $address1;
   public $address2;
   public $city;
   public $state;
   public $phone;
-  public $card_owner;
-  public $owner_id;
+  public $cardOwner;
   public $amount;
   public $comments;
-  public $card_type;
-  public $order_num;
-  public $tx_ref_num;
+  public $cardType;
+  public $txReferenceNumber;
 
-  //provided for debugging purposes
-  public $request_xml;
-  public $response_xml;
+  public $requestXml;
 
   public $transactionMsg;
   public $responseArray;
 
-
-  //Process auth and capture payment
-  public function processACPayment() {
+  /**
+   * Build XML and POST to the Chase Paymentech Orbital Servers.
+   */
+  public function processPayment() {
     $xml = $this->buildXML();
-    $this->SubmitTransaction($xml);
+    $this->submitTransaction($xml);
   }
 
+  /**
+   * Callback to POST to the XML to the Chase Paymentech Orbital servers.
+   */
+  private function submitTransaction($xml) {
 
-  private function SubmitTransaction($xml) {
-
-    $server_environment = variable_get('paymentech_transaction_mode');
-    if($server_environment == 'production') {
-      $URL = "https://orbital1.paymentech.net";
-    } else {
-      $URL = "https://orbitalvar1.paymentech.net";
+    $server_environment = variable_get('uc_orbital_gateway_transaction_mode');
+    if ($server_environment == 'production') {
+      $url = "https://orbital1.paymentech.net";
+    }
+    else {
+      $url = "https://orbitalvar1.paymentech.net";
     }
 
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $URL);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('POST /AUTHORIZE HTTP/1.0','MIME-Version: 1.0', 'Content-type: application/PTI58', 'Document-type: Request', 'Content-transfer-encoding: text', 'Content-length:' . strlen($xml)));
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'POST /AUTHORIZE HTTP/1.0', 'MIME-Version: 1.0',
+      'Content-type: application/PTI58',
+      'Document-type: Request',
+      'Content-transfer-encoding: text',
+      'Content-length:' . strlen($xml),
+    ));
     curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
 
     $this->responsexml = curl_exec($ch);
-    $this->responseArray = $this->ParseXmlResponse($this->responsexml);
-    if (curl_errno($ch))
-    {
-      $this->transactionMsg = "Error in processing xml for curl: " . curl_error($ch)."<br>$xml";
+    $this->responseArray = $this->parseXmlResponse($this->responsexml);
+    if (curl_errno($ch)) {
+      $this->transactionMsg = "Error in processing xml for curl: " . curl_error($ch) . "<br>$xml";
       return curl_errno;
     }
-    else
-    {
+    else {
       curl_close($ch);
     }
   }
 
-
+  /**
+   * Generates the XML formatted to the Orbital Certification documentation.
+   */
   private function buildXML() {
 
-  $server_environment = variable_get('paymentech_transaction_mode');
+    $server_environment = variable_get('uc_orbital_gateway_transaction_mode');
 
-  if($server_environment != 'production') {
-    //default testing values from orbital documentation
-    $xml = '';
-    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
-    $xml .= '<Request>';
-    $xml .= '<NewOrder>';
-    $xml .= '<OrbitalConnectionUsername>'.variable_get('paymentech_orbital_test_username').'</OrbitalConnectionUsername>';
-    $xml .= '<OrbitalConnectionPassword>'.variable_get('paymentech_orbital_test_password').'</OrbitalConnectionPassword>';
-    $xml .= '<IndustryType>'.$this->industry_type.'</IndustryType>';
-    $xml .= '<MessageType>'.$this->message_type.'</MessageType>';
-    $xml .= '<BIN>'.$this->bin.'</BIN>';
-    $xml .= '<MerchantID>'.variable_get('paymentech_merchant_id').'</MerchantID>';
-    $xml .= '<TerminalID>001</TerminalID>';
-    $xml .= '<CardBrand>VI</CardBrand>';
-    $xml .= '<AccountNum>4788250000028291</AccountNum>';
-    $xml .= '<Exp>'.$this->cc_expiry_mon . $this->cc_expiry_yr.'</Exp>';
-    $xml .= '<CurrencyCode>840</CurrencyCode>';
-    $xml .= '<CurrencyExponent>2</CurrencyExponent>';
-    $xml .= '<CardSecVal>'.$this->cvv.'</CardSecVal>';
-    $xml .= '<AVSzip>'.$this->postal_code.'</AVSzip>';
-    $xml .= '<AVSaddress1>'.$this->address1.'</AVSaddress1>';
-    $xml .= '<AVScity>'.$this->city.'</AVScity>';
-    $xml .= '<AVSstate>'.$this->state.'</AVSstate>';
-    $xml .= '<AVSphoneNum>'.$this->phone.'</AVSphoneNum>';
-    $xml .= '<AVSname>'.$this->card_owner.'</AVSname>';
-    $xml .= '<OrderID>'.$this->ordernum.'</OrderID>';
-    $xml .= '<Amount>'.$this->amount.'</Amount>';
-    $xml .= '<CustomerEmail></CustomerEmail>';
-    $xml .= '</NewOrder>';
-    $xml .= '</Request>';
-    $this->request_xml=$xml;
-    } else {
-    $xml = '';
-    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
-    $xml .= '<Request>';
-    $xml .= '<NewOrder>';
-    $xml .= '<OrbitalConnectionUsername>'.variable_get('paymentech_orbital_username').'</OrbitalConnectionUsername>';
-    $xml .= '<OrbitalConnectionPassword>'.variable_get('paymentech_orbital_password').'</OrbitalConnectionPassword>';
-    $xml .= '<IndustryType>'.$this->industry_type.'</IndustryType>';
-    $xml .= '<MessageType>'.$this->message_type.'</MessageType>';
-    $xml .= '<BIN>'.$this->bin.'</BIN>';
-    $xml .= '<MerchantID>'.variable_get('paymentech_merchant_id').'</MerchantID>';
-    $xml .= '<TerminalID>'.variable_get('paymentech_terminal_id').'</TerminalID>';
-    $xml .= '<CardBrand>'.$this->card_type.'</CardBrand>';
-    $xml .= '<AccountNum>'.$this->cc_num.'</AccountNum>';
-    $xml .= '<Exp>'.$this->cc_expiry_mon . $this->cc_expiry_yr.'</Exp>';
-    $xml .= '<CurrencyCode>840</CurrencyCode>';
-    $xml .= '<CurrencyExponent>2</CurrencyExponent>';
-    $xml .= '<CardSecVal>'.$this->cvv.'</CardSecVal>';
-    $xml .= '<AVSzip>'.$this->postal_code.'</AVSzip>';
-    $xml .= '<AVSaddress1>'.$this->address1.'</AVSaddress1>';
-    $xml .= '<AVScity>'.$this->city.'</AVScity>';
-    $xml .= '<AVSstate>'.$this->state.'</AVSstate>';
-    $xml .= '<AVSphoneNum>'.$this->phone.'</AVSphoneNum>';
-    $xml .= '<AVSname>'.$this->card_owner.'</AVSname>';
-    $xml .= '<OrderID>'.$this->ordernum.'</OrderID>';
-    $xml .= '<Amount>'.$this->amount.'</Amount>';
-    $xml .= '<CustomerEmail></CustomerEmail>';
-    $xml .= '</NewOrder>';
-    $xml .= '</Request>';
-    $this->request_xml=$xml;
+    if ($server_environment != 'production') {
+      // Default testing values from Chase orbital certification documentation.
+      $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+      $xml .= '<Request>';
+      $xml .= '<NewOrder>';
+      $xml .= '<OrbitalConnectionUsername>' . variable_get('uc_orbital_gateway_test_username') . '</OrbitalConnectionUsername>';
+      $xml .= '<OrbitalConnectionPassword>' . variable_get('uc_orbital_gateway_test_password') . '</OrbitalConnectionPassword>';
+      $xml .= '<IndustryType>' . $this->industryType . '</IndustryType>';
+      $xml .= '<MessageType>' . $this->messageType . '</MessageType>';
+      $xml .= '<BIN>' . $this->bin . '</BIN>';
+      $xml .= '<MerchantID>' . variable_get('uc_orbital_gateway_merchant_id') . '</MerchantID>';
+      $xml .= '<TerminalID>001</TerminalID>';
+      $xml .= '<CardBrand>VI</CardBrand>';
+      $xml .= '<AccountNum>4788250000028291</AccountNum>';
+      $xml .= '<Exp>' . $this->ccExpireMonth . $this->ccExpireYear . '</Exp>';
+      $xml .= '<CurrencyCode>840</CurrencyCode>';
+      $xml .= '<CurrencyExponent>2</CurrencyExponent>';
+      $xml .= '<CardSecVal>' . $this->cvv . '</CardSecVal>';
+      $xml .= '<AVSzip>' . $this->postalCode . '</AVSzip>';
+      $xml .= '<AVSaddress1>' . $this->address1 . '</AVSaddress1>';
+      $xml .= '<AVScity>' . $this->city . '</AVScity>';
+      $xml .= '<AVSstate>' . $this->state . '</AVSstate>';
+      $xml .= '<AVSphoneNum>' . $this->phone . '</AVSphoneNum>';
+      $xml .= '<AVSname>' . $this->cardOwner . '</AVSname>';
+      $xml .= '<OrderID>' . $this->ordernum . '</OrderID>';
+      $xml .= '<Amount>' . $this->amount . '</Amount>';
+      $xml .= '<CustomerEmail></CustomerEmail>';
+      $xml .= '</NewOrder>';
+      $xml .= '</Request>';
+      $this->requestXml = $xml;
+    }
+    else {
+      $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+      $xml .= '<Request>';
+      $xml .= '<NewOrder>';
+      $xml .= '<OrbitalConnectionUsername>' . variable_get('uc_orbital_gateway_username') . '</OrbitalConnectionUsername>';
+      $xml .= '<OrbitalConnectionPassword>' . variable_get('uc_orbital_gateway_password') . '</OrbitalConnectionPassword>';
+      $xml .= '<IndustryType>' . $this->industryType . '</IndustryType>';
+      $xml .= '<MessageType>' . $this->messageType . '</MessageType>';
+      $xml .= '<BIN>' . $this->bin . '</BIN>';
+      $xml .= '<MerchantID>' . variable_get('uc_orbital_gateway_merchant_id') . '</MerchantID>';
+      $xml .= '<TerminalID>' . variable_get('uc_orbital_gateway_terminal_id') . '</TerminalID>';
+      $xml .= '<CardBrand>' . $this->cardType . '</CardBrand>';
+      $xml .= '<AccountNum>' . $this->ccNumber . '</AccountNum>';
+      $xml .= '<Exp>' . $this->ccExpireMonth . $this->ccExpireYear . '</Exp>';
+      $xml .= '<CurrencyCode>840</CurrencyCode>';
+      $xml .= '<CurrencyExponent>2</CurrencyExponent>';
+      $xml .= '<CardSecVal>' . $this->cvv . '</CardSecVal>';
+      $xml .= '<AVSzip>' . $this->postalCode . '</AVSzip>';
+      $xml .= '<AVSaddress1>' . $this->address1 . '</AVSaddress1>';
+      $xml .= '<AVScity>' . $this->city . '</AVScity>';
+      $xml .= '<AVSstate>' . $this->state . '</AVSstate>';
+      $xml .= '<AVSphoneNum>' . $this->phone . '</AVSphoneNum>';
+      $xml .= '<AVSname>' . $this->cardOwner . '</AVSname>';
+      $xml .= '<OrderID>' . $this->ordernum . '</OrderID>';
+      $xml .= '<Amount>' . $this->amount . '</Amount>';
+      $xml .= '<CustomerEmail></CustomerEmail>';
+      $xml .= '</NewOrder>';
+      $xml .= '</Request>';
+      $this->requestXml = $xml;
     }
     return $xml;
   }
 
-  private function ParseXmlResponse($xmlstring) {
+  /**
+   * Parses out the XML response and returns an array.
+   */
+  private function parseXmlResponse($xmlstring) {
     $xml_parser = xml_parser_create();
-    xml_parser_set_option($xml_parser,XML_OPTION_CASE_FOLDING,0);
-    xml_parser_set_option($xml_parser,XML_OPTION_SKIP_WHITE,1);
+    xml_parser_set_option($xml_parser, XML_OPTION_CASE_FOLDING, 0);
+    xml_parser_set_option($xml_parser, XML_OPTION_SKIP_WHITE, 1);
     xml_parse_into_struct($xml_parser, $xmlstring, $vals, $index);
     xml_parser_free($xml_parser);
 
-    foreach($vals as $val)
-    {
-      $tagval=$val['tag'];
-      if(($val['tag']!='QuickResp') && ($val['tag']!='NewOrderResp')) {
-        if(isset($val['value'])){
-          $newResArr[$tagval]=$val['value'];
-          }
-          else{
-          $newResArr[$tagval]='';
-          }
+    foreach ($vals as $val) {
+      $tag_val = $val['tag'];
+      if (($val['tag'] != 'QuickResp') && ($val['tag'] != 'NewOrderResp')) {
+        if (isset($val['value'])) {
+          $new_response_array[$tag_val] = $val['value'];
         }
+        else {
+          $new_response_array[$tag_val] = '';
+        }
+      }
     }
-        return $newResArr;
+    return $new_response_array;
   }
-}
 
-?>
+}
